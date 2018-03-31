15b2f9ec7fb77eb90ec5a5e5e2036fdef6adbec0
MrMiner <syoa1996@gmail.com>
2018-03-31 22:33:26 +0900

    과제 끝

--- Matrix_02/main.cpp
+++ Matrix_02/main.cpp
@@ -24,23 +24,85 @@ int main( )
 {
 	XMMATRIX A
 	(
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 2.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 4.0f, 0.0f,
-		1.0f, 2.0f, 4.0f, 1.0f
+		1.0f, 0.0f, 4.0f, 0.0f,
+		-2.0f, 3.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
+	);
+	XMMATRIX B
+	(
+		1.0f, 2.0f, 0.0f, 0.0f,
+		2.0f, -4.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
 	);
 
-	XMMATRIX B = XMMatrixIdentity( );
-	XMMATRIX C = A * B;
-	XMMATRIX D = XMMatrixTranspose(A);
-	XMVECTOR det = XMMatrixDeterminant(A);
-	XMMATRIX E = XMMatrixInverse(&det, A);
-	XMMATRIX F = A * E;
+	XMMATRIX C
+	(
+		1.0f, 0.0f, 0.0f, 0.0f,
+		-2.0f, 3.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
+	);
 
-	cout << "A = " << endl << A << endl;
-	cout << "B = " << endl << B << endl;
-	cout << "C = " << endl << C << endl;
-	cout << "D = " << endl << D << endl;
+	XMMATRIX D
+	(
+		1.0f, 2.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 3.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
+	);
+
+	cout << "살려주세요" << endl;
+	cout << "B + C = "	<< endl << B + C << endl;
+	cout << "B - C = "	<< endl << B - C << endl;
+	cout << "B * A = "	<< endl << B * A << endl;
+	cout << "A * D = "	<< endl << A * D << endl;
+	cout << "C * A = "	<< endl << C * A << endl;
+	cout << "A tr = " << endl << XMMatrixTranspose(A) << endl;
+	cout << "B tr = " << endl << XMMatrixTranspose(B) << endl;
+	cout << "C * I = " << endl << C * XMMatrixIsIdentity(C) << endl;
+	cout << "A * 2 = " << endl << A * 2 << endl;
+
+	cout << "==============================" << endl;
+
+	XMMATRIX T
+		(
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			5.0f, -10.0f, 1.0f, 1.0f
+		);
+
+	XMMATRIX Ry = XMMatrixRotationY(45);
+	XMMATRIX S = XMMatrixScaling(3,1,3);
+
+
+	XMMATRIX p
+	(
+		2.0f, 3.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
+	);
+
+
+	XMMATRIX q
+	(
+		3.0f, 1.0f, 1.0f, 1.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
+	);
+
+	cout << "pT = " << endl << p * T << endl;
+	cout << "qT = " << endl << q * T << endl;
+	cout << "pS = " << endl << q * S << endl;
+	cout << "SRT = " << endl << S * Ry * T << endl;
+	cout << "qSRT = " << endl << q * S * Ry * T << endl;
+	cout << "pSRT = " << endl << p * S * Ry * T << endl;
+
+	
 
 	getchar();
 }
